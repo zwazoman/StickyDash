8ea4afce4c9f23a5a7638c6a2a7eefb78f00dafa
zwazoman <gloupik.lemaire@gmail.com>
2024-03-21 11:37:17 +0100

    Dashscript réparé

--- Assets/Scripts/Infos.cs
+++ Assets/Scripts/Infos.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Infos : MonoBehaviour
 {
     [SerializeField] int _dashLimit;
+    [SerializeField] TMP_Text dashLimitText;
     Spawn spawn;
     Dash dash;
 
     private void Awake()
     {
         spawn = GetComponent<Spawn>();
+    }
+    private void Start()
+    {
+        dash = spawn.player.GetComponent<Dash>();
+        dash.dashLimit = _dashLimit;
+        dashLimitText.text = _dashLimit.ToString();
     }
 
     public void Respawn()
